17dd84e0ad6629f5d1c87405cd4291eb305cd485
Madars Batraks <madarsbatraks@gmail.com>
2023-09-22 12:03:17 +0300

    removed the fucking comma

--- commands/ping.js
+++ commands/ping.js
@@ -8,5 +8,5 @@ module.exports = {
         const diff = m.createdTimestamp - msg.createdTimestamp
         const ping = client.ws.ping
         m.edit(`Pong! 🏓 (Round trip took: ${diff}ms. Heartbeat: ${ping}ms.)`);
-    },
+    }
 }
