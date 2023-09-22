db8541ba1088417b26a2c1a3460653c4f1e01c2d
Madars Batraks <madarsbatraks@gmail.com>
2023-09-22 12:01:53 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,10 @@ module.exports = {
         name: "ping",
         description: "Get ponged"
     },
-    async run(msg) {
-        msg.reply("pong!")
+    async run(msg, client) {
+        const m = await msg.channel.send("Ping?");
+        const diff = m.createdTimestamp - msg.createdTimestamp
+        const ping = client.ws.ping
+        m.edit(`Pong! 🏓 (Round trip took: ${diff}ms. Heartbeat: ${ping}ms.)`);
     },
 }
